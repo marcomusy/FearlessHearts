a3f2074e0df0b37891a0ba52a38fed213aad970a
Giovanni Dalmasso <gio.dalmasso@gmail.com>
2021-12-13 12:58:37 +0100

    formatting

--- heart_database.py
+++ heart_database.py
@@ -5,9 +5,9 @@ from scipy.interpolate import splev, splrep
 
 data_path_wt = 'data/wt/'
 data_path_ko = 'data/ko/'
-grid_res     = 50
-radius_res   = 50
-nsomites     = [10, 14, 18.5, 21.5, 24.5, 28.5]
+grid_res = 50
+radius_res = 50
+nsomites = [10, 14, 18.5, 21.5, 24.5, 28.5]
 
 dataset_ko = dict(
 
@@ -15,88 +15,88 @@ dataset_ko = dict(
 
     ray_points=[],
 
-    timecourse = dict(files=[
-                                data_path_ko+'LQR_M_JJ_1819_ko_3',
-                                data_path_ko+'LQR_M_JJ_2122_ko_1',
-                                data_path_ko+'LQR_M_JJ_2425_ko_1',
-                                data_path_ko+'LQR_M_JJ_2829_ko_2',
-                                data_path_ko+'heart_290113_14sp_ko_3_c2',
-                                data_path_ko+'heart_300113_10sp_ko_2_c2',
-                    ],
-                    ref_nr=0,
-        ),
-
-    set10 = dict( nsomites=10,
-                  files = [
-                            data_path_ko+'heart_300113_10sp_ko_1_c2',
-                            data_path_ko+'heart_300113_10sp_ko_2_c2',
-                            data_path_ko+'heart_300113_10sp_ko_3_c2',
-                           ],
-                  ray_distances=[],
-                  clms=[], # list of length = nr of files
-                  comment='',
-                  ref_nr=1,
+    timecourse=dict(files=[
+        data_path_ko+'LQR_M_JJ_1819_ko_3',
+        data_path_ko+'LQR_M_JJ_2122_ko_1',
+        data_path_ko+'LQR_M_JJ_2425_ko_1',
+        data_path_ko+'LQR_M_JJ_2829_ko_2',
+        data_path_ko+'heart_290113_14sp_ko_3_c2',
+        data_path_ko+'heart_300113_10sp_ko_2_c2',
+    ],
+        ref_nr=0,
     ),
 
-    set14 = dict( nsomites=14,
-                  files = [
-                            data_path_ko+'heart_110113_14sp_ko_1_c2',
-                            data_path_ko+'heart_110113_14sp_ko_2_c2',
-                            data_path_ko+'heart_290113_14sp_ko_3_c2',
-                           ],
-                  ray_distances=[],
-                  clms=[], # list of length = nr of files
-                  comment='',
-                  ref_nr=2,
-    ),
+    set10=dict(nsomites=10,
+               files=[
+                   data_path_ko+'heart_300113_10sp_ko_1_c2',
+                   data_path_ko+'heart_300113_10sp_ko_2_c2',
+                   data_path_ko+'heart_300113_10sp_ko_3_c2',
+               ],
+               ray_distances=[],
+               clms=[],  # list of length = nr of files
+               comment='',
+               ref_nr=1,
+               ),
 
-    set1819 = dict( nsomites=18.5,
-                    files = [
-                              data_path_ko+'LQR_M_JJ_1819_ko_1',
-                              data_path_ko+'LQR_M_JJ_1819_ko_2',
-                              data_path_ko+'LQR_M_JJ_1819_ko_3',
-                            ],
-                    ray_distances=[],
-                    clms=[], # list of length = nr of files
-                    comment='',
-                    ref_nr=2,
-    ),
+    set14=dict(nsomites=14,
+               files=[
+                   data_path_ko+'heart_110113_14sp_ko_1_c2',
+                   data_path_ko+'heart_110113_14sp_ko_2_c2',
+                   data_path_ko+'heart_290113_14sp_ko_3_c2',
+               ],
+               ray_distances=[],
+               clms=[],  # list of length = nr of files
+               comment='',
+               ref_nr=2,
+               ),
 
-    set2122 = dict( nsomites=21.5,
-                    files = [
-                              data_path_ko+'LQR_M_JJ_2122_ko_1',
-                              data_path_ko+'LQR_M_JJ_2122_ko_2',
-                              data_path_ko+'LQR_M_JJ_2122_ko_3',
-                              data_path_ko+'LQR_M_JJ_2122_ko_4',
-                             ],
-                    ray_distances=[],
-                    clms=[], # list of length = nr of files
-                    comment='',
-                    ref_nr=0,
-    ),
+    set1819=dict(nsomites=18.5,
+                 files=[
+                     data_path_ko+'LQR_M_JJ_1819_ko_1',
+                     data_path_ko+'LQR_M_JJ_1819_ko_2',
+                     data_path_ko+'LQR_M_JJ_1819_ko_3',
+                 ],
+                 ray_distances=[],
+                 clms=[],  # list of length = nr of files
+                 comment='',
+                 ref_nr=2,
+                 ),
 
-    set2425 = dict( nsomites=24.5,
-                    files = [
-                              data_path_ko+'LQR_M_JJ_2425_ko_1',
-                              data_path_ko+'LQR_M_JJ_2425_ko_3',
-                             ],
-                    ray_distances=[],
-                    clms=[], # list of length = nr of files
-                    comment='',
-                    ref_nr=0,
-    ),
+    set2122=dict(nsomites=21.5,
+                 files=[
+                     data_path_ko+'LQR_M_JJ_2122_ko_1',
+                     data_path_ko+'LQR_M_JJ_2122_ko_2',
+                     data_path_ko+'LQR_M_JJ_2122_ko_3',
+                     data_path_ko+'LQR_M_JJ_2122_ko_4',
+                 ],
+                 ray_distances=[],
+                 clms=[],  # list of length = nr of files
+                 comment='',
+                 ref_nr=0,
+                 ),
 
-    set2829 = dict( nsomites=28.5,
-                    files = [
-                              data_path_ko+'LQR_M_JJ_2829_ko_1',
-                              data_path_ko+'LQR_M_JJ_2829_ko_2',
-                              data_path_ko+'LQR_M_JJ_2829_ko_3',
-                             ],
-                    ray_distances=[],
-                    clms=[], # list of length = nr of files
-                    comment='',
-                    ref_nr=1,
-    ),
+    set2425=dict(nsomites=24.5,
+                 files=[
+                     data_path_ko+'LQR_M_JJ_2425_ko_1',
+                     data_path_ko+'LQR_M_JJ_2425_ko_3',
+                 ],
+                 ray_distances=[],
+                 clms=[],  # list of length = nr of files
+                 comment='',
+                 ref_nr=0,
+                 ),
+
+    set2829=dict(nsomites=28.5,
+                 files=[
+                     data_path_ko+'LQR_M_JJ_2829_ko_1',
+                     data_path_ko+'LQR_M_JJ_2829_ko_2',
+                     data_path_ko+'LQR_M_JJ_2829_ko_3',
+                 ],
+                 ray_distances=[],
+                 clms=[],  # list of length = nr of files
+                 comment='',
+                 ref_nr=1,
+                 ),
 )
 
 ##########################################################################
@@ -106,103 +106,104 @@ dataset_wt = dict(
 
     ray_points=[],
 
-    timecourse = dict(files=[
-                                data_path_wt+'LQR_M_JJ_1819_wt_2', #
-                                data_path_wt+'LQR_M_JJ_2122_wt_1',
-                                data_path_wt+'LQR_M_JJ_2425_wt_2b',
-                                data_path_wt+'LQR_M_JJ_2829_wt_3',
-                                data_path_wt+'heart_110113_14sp_wt_2_c2',
-                                data_path_wt+'heart_300113_10sp_wt_4_c2',
-                            ],
-                        ref_nr=0,
+    timecourse=dict(files=[
+        data_path_wt+'LQR_M_JJ_1819_wt_2',
+        data_path_wt+'LQR_M_JJ_2122_wt_1',
+        data_path_wt+'LQR_M_JJ_2425_wt_2b',
+        data_path_wt+'LQR_M_JJ_2829_wt_3',
+        data_path_wt+'heart_110113_14sp_wt_2_c2',
+        data_path_wt+'heart_300113_10sp_wt_4_c2',
+    ],
+        ref_nr=0,
     ),
 
-    set10 = dict(
-                   nsomites=10,
-                   files = [
-                            data_path_wt+'heart_300113_10sp_wt_1_c2',
-                            data_path_wt+'heart_300113_10sp_wt_3_c2',
-                            data_path_wt+'heart_300113_10sp_wt_4_c2', #
-                            data_path_wt+'heart_300113_10sp_wt_5_c2',
-                           ],
-                   ray_distances=[],
-                   clms=[], # list of length = nr of files
-                   comment='',
-                   ref_nr=2,
+    set10=dict(
+        nsomites=10,
+        files=[
+            data_path_wt+'heart_300113_10sp_wt_1_c2',
+            data_path_wt+'heart_300113_10sp_wt_3_c2',
+            data_path_wt+'heart_300113_10sp_wt_4_c2',
+            data_path_wt+'heart_300113_10sp_wt_5_c2',
+        ],
+        ray_distances=[],
+        clms=[],  # list of length = nr of files
+        comment='',
+        ref_nr=2,
     ),
 
-    set14 = dict(
-                   nsomites=14,
-                   files = [
-                            data_path_wt+'heart_110113_14sp_wt_1_c2',
-                            data_path_wt+'heart_110113_14sp_wt_2_c2', #
-                            data_path_wt+'heart_300113_14sp_wt_3_c2',
-                            data_path_wt+'heart_300113_14sp_wt_4_c2',
-                           ],
-                   ray_distances=[],
-                   clms=[],
-                   comment='',
-                   ref_nr=1,
+    set14=dict(
+        nsomites=14,
+        files=[
+            data_path_wt+'heart_110113_14sp_wt_1_c2',
+            data_path_wt+'heart_110113_14sp_wt_2_c2',
+            data_path_wt+'heart_300113_14sp_wt_3_c2',
+            data_path_wt+'heart_300113_14sp_wt_4_c2',
+        ],
+        ray_distances=[],
+        clms=[],
+        comment='',
+        ref_nr=1,
     ),
 
-    set1819 = dict(
-                   nsomites=18.5,
-                   files = [
-                            data_path_wt+'LQR_M_JJ_1819_wt_1',
-                            data_path_wt+'LQR_M_JJ_1819_wt_2', #
-                            data_path_wt+'LQR_M_JJ_1819_wt_3',
-                            data_path_wt+'LQR_M_JJ_1819_wt_4',
-                            data_path_wt+'LQR_M_JJ_1819_wt_5',
-                           ],
-                   ray_distances=[],
-                   clms=[],
-                   comment='',
-                   ref_nr=1,
+    set1819=dict(
+        nsomites=18.5,
+        files=[
+            data_path_wt+'LQR_M_JJ_1819_wt_1',
+            data_path_wt+'LQR_M_JJ_1819_wt_2',
+            data_path_wt+'LQR_M_JJ_1819_wt_3',
+            data_path_wt+'LQR_M_JJ_1819_wt_4',
+            data_path_wt+'LQR_M_JJ_1819_wt_5',
+        ],
+        ray_distances=[],
+        clms=[],
+        comment='',
+        ref_nr=1,
     ),
 
-    set2122 = dict(
-                   nsomites=21.5,
-                   files = [
-                            data_path_wt+'LQR_M_JJ_2122_wt_1', #
-                            data_path_wt+'LQR_M_JJ_2122_wt_2',
-                            data_path_wt+'LQR_M_JJ_2122_wt_3',
-                            data_path_wt+'LQR_M_JJ_2122_wt_4',
-                            data_path_wt+'LQR_M_JJ_2122_wt_5',
-                           ],
-                   ray_distances=[],
-                   clms=[],
-                   comment='',
-                   ref_nr=0,
+    set2122=dict(
+        nsomites=21.5,
+        files=[
+            data_path_wt+'LQR_M_JJ_2122_wt_1',
+            data_path_wt+'LQR_M_JJ_2122_wt_2',
+            data_path_wt+'LQR_M_JJ_2122_wt_3',
+            data_path_wt+'LQR_M_JJ_2122_wt_4',
+            data_path_wt+'LQR_M_JJ_2122_wt_5',
+        ],
+        ray_distances=[],
+        clms=[],
+        comment='',
+        ref_nr=0,
     ),
 
-     set2425 = dict(
-                   nsomites=24.5,
-                   files = [
-                            data_path_wt+'LQR_M_JJ_2425_wt_1b',
-                            data_path_wt+'LQR_M_JJ_2425_wt_2b', #
-                            data_path_wt+'LQR_M_JJ_2425_wt_3b',
-                           ],
-                   ray_distances=[],
-                   clms=[],
-                   comment='',
-                   ref_nr=1,
+    set2425=dict(
+        nsomites=24.5,
+        files=[
+            data_path_wt+'LQR_M_JJ_2425_wt_1b',
+            data_path_wt+'LQR_M_JJ_2425_wt_2b',
+            data_path_wt+'LQR_M_JJ_2425_wt_3b',
+        ],
+        ray_distances=[],
+        clms=[],
+        comment='',
+        ref_nr=1,
     ),
 
-     set2829 = dict(
-                   nsomites=28.5,
-                   files = [
-                            data_path_wt+'LQR_M_JJ_2829_wt_1',
-                            data_path_wt+'LQR_M_JJ_2829_wt_2',
-                            data_path_wt+'LQR_M_JJ_2829_wt_3', #
-                            data_path_wt+'LQR_M_JJ_E95_2829_wt_4',
-                            data_path_wt+'LQR_M_JJ_E95_2829_wt_5',
-                           ],
-                   ray_distances=[],
-                   clms=[],
-                   comment='',
-                   ref_nr=2,
+    set2829=dict(
+        nsomites=28.5,
+        files=[
+            data_path_wt+'LQR_M_JJ_2829_wt_1',
+            data_path_wt+'LQR_M_JJ_2829_wt_2',
+            data_path_wt+'LQR_M_JJ_2829_wt_3',
+            data_path_wt+'LQR_M_JJ_E95_2829_wt_4',
+            data_path_wt+'LQR_M_JJ_E95_2829_wt_5',
+        ],
+        ray_distances=[],
+        clms=[],
+        comment='',
+        ref_nr=2,
     ),
 )
+
 
 def spline(x, y, res, smooth):
     """
@@ -212,4 +213,3 @@ def spline(x, y, res, smooth):
     xnew = np.linspace(x[0], x[-1], res)
     ynew = splev(xnew, splrep(x, y, s=smooth), der=0)
     return xnew, ynew
-
